0bc71f5675cf141070d7fb0e2e4e97d702460922
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2022-11-30 20:12:51 -0500

    commit tree e68c63f21b772caf83fce983bb51d1257eede587

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    function clickAlert() {
+        return alert('I was clicked!');
+        }
+   return input.addEventListener('click', clickAlert);
 }
+addingEventListener();
